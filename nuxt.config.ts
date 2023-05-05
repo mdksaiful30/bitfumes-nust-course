c0d0c8442c0727935c2de0463d7fbd9b1ae94731
mdksaiful30 <mdksaiful12@gmail.com>
2023-05-05 22:41:28 +0600

    added layouts, pages, content and tailwind css

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-export default defineNuxtConfig({
 
-})
+export default defineNuxtConfig({
+    css: ['~/assets/css/tailwind.css'],
+    modules: [
+      '@nuxt/content'
+    ],
+    postcss: {
+        plugins: {
+          tailwindcss: {},
+          autoprefixer: {},
+        },
+      },
+  })
+
+  
+  
---
+++ pages/about.vue
@@ -0,0 +1,67 @@
+
+<template>
+    <!-- Navbar goes here -->
+	<nav class="bg-white shadow-lg">
+		<div class="max-w-6xl mx-auto px-4">
+			<div class="flex justify-between">
+				<div class="flex space-x-7">
+					<!-- Website Logo -->
+					<div>
+						<a href="#" class="flex items-center py-4 px-2">
+							<img src="../logo.svg" alt="Logo" class="h-8 w-8 mr-2" />
+							<span class="font-semibold text-gray-500 text-lg"
+								>Navigation</span
+							>
+						</a>
+					</div>
+					<!-- Primary Navbar items -->
+					<div class="hidden md:flex items-center space-x-1">
+						<a
+							href="/"
+							class="py-4 px-2 text-gray-500 font-semibold hover:text-green-500 transition duration-300 "
+							>Home</a
+						>
+						<a
+							href=""
+							class="py-4 px-2 text-gray-500 font-semibold hover:text-green-500 transition duration-300"
+							>Services</a
+						>
+						<a
+							href=""
+							class="py-4 px-2 text-green-500 border-b-4 border-green-500 font-semibold"
+							>About</a
+						>
+						<a
+							href=""
+							class="py-4 px-2 text-gray-500 font-semibold hover:text-green-500 transition duration-300"
+							>Contact Us</a
+						>
+					</div>
+				</div>
+			</div>
+		</div>
+	</nav>
+  <!-- Mobile menu button -->
+
+<div class="md:hidden flex items-center">
+	<button class="outline-none mobile-menu-button">
+		<svg
+			class="w-6 h-6 text-gray-500"
+			x-show="!showMenu"
+			fill="none"
+			stroke-linecap="round"
+			stroke-linejoin="round"
+			stroke-width="2"
+			viewBox="0 0 24 24"
+			stroke="currentColor"
+		>
+		<path d="M4 6h16M4 12h16M4 18h16"></path>
+		</svg>
+	</button>
+</div>
+
+<div class="flex">
+    <h1>About page</h1>
+</div>
+  </template>
+  
